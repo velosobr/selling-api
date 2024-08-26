00a147b47f34c5c7f005ea8bfbb660333def7e16
velosobr <linoc.veloso@gmail.com>
2024-08-25 22:28:17 -0300

    Changes to be committed: 	modified:   package.json 	new file:   src/shared/errors/AppError.ts 	modified:   src/shared/http/server.ts 	modified:   yarn.lock

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
+import AppError from "@shared/errors/AppError";
 
 const app = express();
 
@@ -8,6 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+// Middleware to handle errors
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: "error",
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: "error",
+            message: "Internal Server Error",
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log("Server is running on port 3333 🚀");
